d182d6949fa456637d70a4052cd9298ebf289799
Payam <thisispayam@gmail.com>
2018-03-15 10:09:29 -0400

    Securing routing with Authorization - added navbar, and other pages and protecting it with guards for admin and subscribers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {AppComponent} from './app.component';
 import {LoginPageComponent} from './login-page/login-page.component';
-import {PagesListComponent} from './pages-list/pages-list.component'
+import {PagesListComponent} from './pages-list/pages-list.component';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AdminGuard } from './guards/admin.guard';
+import { SubscriberGuard} from './guards/subscriber.guard';
+
 
 const routes: Routes = [
-  {path: '', component: PagesListComponent},
-  {path: 'login', component: LoginPageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'article', component:PagesListComponent, canActivate: [SubscriberGuard]},
+  {path: 'admin', component: AdminPageComponent, canActivate: [AdminGuard]}
 ]
 
 @NgModule({
